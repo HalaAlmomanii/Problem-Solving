833be32fdf5c05332a510d181881e4155ee63c0e
Hala Almomani <you@example.com>
2019-11-12 12:18:12 +0200

    Q15

--- App.js
+++ App.js
@@ -462,10 +462,6 @@
 
 
 //   function reverse(x) {
-
-      
-      
-  
     
 //   let first=x
 //    x=x.toString()
@@ -484,3 +480,50 @@
   
 // };
 // }
+
+// console.log(reverse('123'))
+
+
+
+// / // -----------------------------------------------------------------
+
+
+// // Q15 Search Insert Position
+// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You may assume no duplicates in the array.
+
+// Example 1:
+
+// Input: [1,3,5,6], 5
+// Output: 2
+// Example 2:
+
+// Input: [1,3,5,6], 2
+// Output: 1
+
+// function searchInsert(nums, target) {
+//     let index
+    
+// for(i=0;i<nums.length;i++)
+//     {
+//         if(nums[i]===target)
+//         return i
+//     }
+   
+//     for(i=0;i<nums.length;i++)
+//         {
+//             if(target<nums[0])
+//                 return 0
+//             if(target>nums[i] && target<nums[i+1])
+//                 return i+1
+//             if(target>nums[nums.length-1])
+//                 return nums.length
+            
+//         }
+        
+        
+    
+// }
+
+// console.log(searchInsert([1,2,4],3))
