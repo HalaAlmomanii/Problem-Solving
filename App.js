cbc238656a3e340eedb1efdd8ba32b3b12d60687
Hala Almomani <you@example.com>
2019-11-18 12:08:43 +0200

    Q18

--- App.js
+++ App.js
@@ -677,4 +677,45 @@
 // return count*-1
 
 // };
-// console.log(divide(10,3)) 
+// console.log(divide(10,3))
+
+
+// / // -----------------------------------------------------------------
+// Q18 First Missing Positive
+// Given an unsorted integer array, find the smallest missing positive integer.
+
+// Example 1:
+// Input: [1,2,0]
+// Output: 3
+// Example 2:
+// Input: [3,4,-1,1]
+// Output: 2
+// Example 3:
+// Input: [7,8,9,11,12]
+// Output: 1
+
+//    function firstMissingPositive (nums) {
+
+//     let x
+    
+//     let myarr=nums.sort(function(a, b){return a - b})
+  
+//     if(nums.length===0)
+//         return 1
+    
+//     for(i=1;i<=myarr.length+1;i++)
+//         {
+//             if(myarr.includes(i))
+//                 {
+                    
+//                 }
+            
+//                 else
+//                  { x= i
+//                  break ;
+//                  }
+                
+//         }
+//     return x
+
+//    }
