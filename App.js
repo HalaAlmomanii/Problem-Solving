5fbf83e932251b087037e1b39966c50d6d4b4b6f
Hala Almomani <you@example.com>
2019-11-20 09:48:50 +0200

    Q19

--- App.js
+++ App.js
@@ -719,3 +719,35 @@
 //     return x
 
 //    }
+
+
+// / // -----------------------------------------------------------------
+
+// Q19 Search in Rotated Sorted Array
+
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+// You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+// You may assume no duplicate exists in the array.
+
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+
+
+// function search(nums, target) {
+  
+//     for(i=0;i<nums.length;i++)
+//         {
+//     if(target===nums[i])
+//         return i
+//         }
+    
+//         return -1
+// };
+// console.log(search([4,5,6,7,0,1,2])
